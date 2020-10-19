8ea7ae4aed37086290068719bf0d0fa60910cb8f
ola.pribylskaya <ola.pribylskaya@mail.ru>
2020-10-19 11:45:56 +0300

    Новая проверка трапеции

--- MyLib/Trapeciya.cs
+++ MyLib/Trapeciya.cs
@@ -29,13 +29,27 @@ namespace MyLib
             return Math.Sqrt(Math.Pow(Convert.ToDouble(x2) - Convert.ToDouble(x1), 2) + Math.Pow(Convert.ToDouble(y2) - Convert.ToDouble(y1), 2));
         }
 
-        // проверка , что фигура является трапецией 
+        // проверка , что фигура является трапецией m1*p2 = p1*m2 (параллельны)
         public static bool proverka(string[] param)
         {
-            if ((Convert.ToInt32(param[1]) == Convert.ToInt32(param[3]) && Convert.ToInt32(param[5]) == Convert.ToInt32(param[7])) || 
-                (Convert.ToInt32(param[0]) == Convert.ToInt32(param[2]) && Convert.ToInt32(param[4]) == Convert.ToInt32(param[6]))) // две стороны параллельны
+            int bm1 = Convert.ToInt32(param[4]) - Convert.ToInt32(param[2]);
+            int bm2 = Convert.ToInt32(param[0]) - Convert.ToInt32(param[6]);
+            int bp1 = Convert.ToInt32(param[5]) - Convert.ToInt32(param[3]);
+            int bp2 = Convert.ToInt32(param[1]) - Convert.ToInt32(param[7]);
+
+            int om1 = Convert.ToInt32(param[2]) - Convert.ToInt32(param[0]);
+            int om2 = Convert.ToInt32(param[4]) - Convert.ToInt32(param[6]);
+            int op1 = Convert.ToInt32(param[3]) - Convert.ToInt32(param[1]);
+            int op2 = Convert.ToInt32(param[5]) - Convert.ToInt32(param[7]);
+
+            if (bm1*bp2 != bp1*bm2 && om1*op2 == op1*om2 )
                 return true; // это трапеция
             else return false;
+
+            /*if ((Convert.ToInt32(param[1]) == Convert.ToInt32(param[3]) && Convert.ToInt32(param[5]) == Convert.ToInt32(param[7])) || 
+                (Convert.ToInt32(param[0]) == Convert.ToInt32(param[2]) && Convert.ToInt32(param[4]) == Convert.ToInt32(param[6]))) // две стороны параллельны
+                return true; // это трапеция
+            else return false;*/
         }
 
         //периметр трапеции P = a + b + c + d
